ce942b62293b569d328d9a52913ef40170ecc9a8
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-09-05 15:13:57 +0200

    Updated the examples to account for the latest changes on forage

--- run-multi-agent.sh
+++ run-multi-agent.sh
@@ -1,1 +1,6 @@
-camel run multi-agent.camel.yaml --dep=mvn:org.apache.camel.forage:forage-agent-simple:1.0-SNAPSHOT --dep=mvn:org.apache.camel.forage:forage-memory-message-window:1.0-SNAPSHOT --dep=mvn:org.apache.camel.forage:forage-agent-memoryless:1.0-SNAPSHOT --dep=mvn:org.apache.camel.forage:forage-model-google-gemini:1.0-SNAPSHOT --dep=mvn:org.apache.camel.forage:forage-model-ollama:1.0-SNAPSHOT --dep=camel-langchain4j-agent
+camel run multi-agent.camel.yaml \
+	--dep=mvn:org.apache.camel.forage:forage-agent:1.0-SNAPSHOT \
+	--dep=mvn:org.apache.camel.forage:forage-memory-message-window:1.0-SNAPSHOT \
+	--dep=mvn:org.apache.camel.forage:forage-model-google-gemini:1.0-SNAPSHOT \
+	--dep=mvn:org.apache.camel.forage:forage-model-ollama:1.0-SNAPSHOT \
+	--dep=camel-langchain4j-agent
--- run-single-agent.sh
+++ run-single-agent.sh
@@ -1,1 +1,5 @@
-camel run * --dep=mvn:org.apache.camel.forage:forage-agent-simple:1.0-SNAPSHOT --dep=mvn:org.apache.camel.forage:forage-memory-message-window:1.0-SNAPSHOT --dep=mvn:org.apache.camel.forage:forage-model-google-gemini:1.0-SNAPSHOT --dep=camel-langchain4j-agent
+camel run * \
+	--dep=mvn:org.apache.camel.forage:forage-agent:1.0-SNAPSHOT \
+	--dep=mvn:org.apache.camel.forage:forage-memory-message-window:1.0-SNAPSHOT \
+	--dep=mvn:org.apache.camel.forage:forage-model-google-gemini:1.0-SNAPSHOT \
+	--dep=camel-langchain4j-agent
