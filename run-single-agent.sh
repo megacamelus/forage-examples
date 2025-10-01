834f5ef2304b1d72880fa696a6cf409b585fcfc5
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-10-01 10:33:42 +0200

    Fixed the run single agent command

--- run-single-agent.sh
+++ run-single-agent.sh
@@ -1,4 +1,4 @@
-camel run * \
+camel run agent.camel.yaml \
 	--dep=mvn:org.apache.camel.forage:forage-agent:1.0-SNAPSHOT \
 	--dep=mvn:org.apache.camel.forage:forage-memory-message-window:1.0-SNAPSHOT \
 	--dep=mvn:org.apache.camel.forage:forage-model-google-gemini:1.0-SNAPSHOT \
