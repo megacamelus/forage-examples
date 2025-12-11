ddc5c4f6a9f18077b1c331defec85d8733411759
Croway <federico.mariani.1990@gmail.com>
2025-12-11 18:23:30 +0100

    Use created Agent beans

--- ai/multi/export-multi-agent-to-csb.sh
+++ ai/multi/export-multi-agent-to-csb.sh
@@ -1,13 +1,13 @@
-camel export --runtime=quarkus --quarkus-version=3.27.0.CR1 \
-	--dir=ceq-example multi-agent.camel.yaml \
+camel export --runtime=spring-boot \
+	--dir=csb-example multi-agent.camel.yaml forage-agent-factory.properties forage-model-google-gemini.properties forage-model-ollama.properties \
 	--dep=mvn:org.apache.camel.forage:forage-agent:1.0-SNAPSHOT \
 	--dep=mvn:org.apache.camel.forage:forage-memory-message-window:1.0-SNAPSHOT \
 	--dep=mvn:org.apache.camel.forage:forage-model-google-gemini:1.0-SNAPSHOT \
 	--dep=mvn:org.apache.camel.forage:forage-model-ollama:1.0-SNAPSHOT \
 	--dep=camel-langchain4j-agent
 
-	cp forage* ceq-example
+cp csb-example
 
 # then, to run:
-# cd ceq-example
-# mvn -Dcamel.version=4.14.1-SNAPSHOT quarkus:dev
+# cd csb-example
+# mvn
--- ai/single/run-single-agent.sh
+++ ai/single/run-single-agent.sh
@@ -1,6 +1,5 @@
-camel run multi-agent.camel.yaml \
+camel run agent.camel.yaml forage-model-ollama.properties \
 	--dep=mvn:org.apache.camel.forage:forage-agent:1.0-SNAPSHOT \
 	--dep=mvn:org.apache.camel.forage:forage-memory-message-window:1.0-SNAPSHOT \
-	--dep=mvn:org.apache.camel.forage:forage-model-google-gemini:1.0-SNAPSHOT \
 	--dep=mvn:org.apache.camel.forage:forage-model-ollama:1.0-SNAPSHOT \
 	--dep=camel-langchain4j-agent
