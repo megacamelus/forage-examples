39b040be308bf74e25a8bbc486bfa59942e62922
Croway <federico.mariani.1990@gmail.com>
2025-12-15 15:37:24 +0100

    Use ForageAgent Beans

--- ai/multi/export-multi-agent-to-ceq.sh
+++ ai/multi/export-multi-agent-to-ceq.sh
@@ -1,5 +1,5 @@
 camel export --runtime=quarkus \
-	--dir=ceq-example multi-agent.camel.yaml forage-agent-factory.properties forage-model-google-gemini.properties forage-model-ollama.properties \
+	--dir=ceq-example multi-agent.camel.yaml forage-agent-factory.properties \
 	--dep=mvn:org.apache.camel.forage:forage-agent:1.0-SNAPSHOT \
 	--dep=mvn:org.apache.camel.forage:forage-memory-message-window:1.0-SNAPSHOT \
 	--dep=mvn:org.apache.camel.forage:forage-model-google-gemini:1.0-SNAPSHOT \
--- ai/single/run-single-agent.sh
+++ ai/single/run-single-agent.sh
@@ -1,4 +1,4 @@
-camel run agent.camel.yaml forage-model-ollama.properties \
+camel run agent.camel.yaml forage-agent-factory.properties \
 	--dep=mvn:org.apache.camel.forage:forage-agent:1.0-SNAPSHOT \
 	--dep=mvn:org.apache.camel.forage:forage-memory-message-window:1.0-SNAPSHOT \
 	--dep=mvn:org.apache.camel.forage:forage-model-ollama:1.0-SNAPSHOT \
